41f9d523a1e434c1a8eb0f8997f0fceb9d6cf0e3
malak <malakmaher272@gmail.com>
2025-10-28 22:49:35 +0300

    Operators_Expressions.

---
+++ Operators_Expressions.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    // Arithmetic Operators
+    cout << "a + b = " << a + b << endl; // Addition
+    cout << "a - b = " << a - b << endl; // Subtraction
+    cout << "a * b = " << a * b << endl; // Multiplication
+    cout << "a / b = " << a / b << endl; // Division
+    cout << "a % b = " << a % b << endl; // Modulus
+
+    // Relational Operators
+    cout << "a == b: " << (a == b) << endl; // Equal to
+    cout << "a != b: " << (a != b) << endl; // Not equal to
+    cout << "a > b: " << (a > b) << endl;   // Greater than
+    cout << "a < b: " << (a < b) << endl;   // Less than
+    cout << "a >= b: " << (a >= b) << endl; // Greater than or equal to
+    cout << "a <= b: " << (a <= b) << endl; // Less than or equal to
+
+    // Logical Operators
+    bool x = true;
+    bool y = false;
+    cout << "x && y: " << (x && y) << endl; // Logical AND
+    cout << "x || y: " << (x || y) << endl; // Logical OR
+    cout << "!x: " << (!x) << endl;         // Logical NOT
+
+    return 0;
+}
+
---
+++ Variables_Data_Types.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    string name, city; 
+    
+    int age; // 4 bytes , 32 bits
+
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your city: ";
+    cin >> city;
+    cout << "Hello, " << name << "! You are " << age << " years old and live in " << city << "." << endl;
+    
+
+}
+    
+
